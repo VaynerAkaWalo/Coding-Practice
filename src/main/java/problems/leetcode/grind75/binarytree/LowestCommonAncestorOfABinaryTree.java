68dfae1721026f9c5c6f261512977df2b3d5068e
VaynerAkaWalo <vaynerakawalo@gmail.com>
2023-11-12 21:41:49 +0100

    Lowest common ancestor of a binary tree

---
+++ src/main/java/problems/leetcode/grind75/binarytree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,17 @@
+package problems.leetcode.grind75.binarytree;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LowestCommonAncestorOfABinaryTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (p == root || q == root) return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left == null ? right : left;
+    }
+}
